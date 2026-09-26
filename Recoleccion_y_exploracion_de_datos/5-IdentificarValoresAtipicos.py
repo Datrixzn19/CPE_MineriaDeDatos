cfb59be448c5565fa13051eb29ba51b5794eca54
David19 <davidleonel911@gmail.com>
2026-09-25 19:25:35 -0500

    Identificacion de valores atipicos

--- Recoleccion_y_exploracion_de_datos/5-IdentificarValoresAtipicos.py
+++ Recoleccion_y_exploracion_de_datos/5-IdentificarValoresAtipicos.py
@@ -5,12 +5,10 @@ df = pd.read_csv("Mental_Health_in_Tech_Survey.csv")
 limite_inferior = 15
 limite_superior = 100
 
-
 atipicos = df[(df['Age'] < limite_inferior) | (df['Age'] > limite_superior)]# Trae las filas donde la Edad sea MENOR a 15 O MAYOR a 100".
-
 
 cantidad_atipicos = len(atipicos) #cuenta la cantidad de filas del DataFrame 'atipicos'
 
 print(f"Cantidad de valores atípicos detectados: {cantidad_atipicos}")
 print("Valores exactos encontrados:")
-print(atipicos['Age'].values)#extrae solo los números en forma de lista, ignorando el resto de las columnas.
+print(atipicos['Age'].values)#extrae solo los números en forma de lista, ignorando el resto de las columnas
